1b530c539ebb02203a7e69f0f39ce96aa9ce7a36
Gaurav <35771409+gauravgupta45@users.noreply.github.com>
2020-02-21 18:36:11 +0530

    Update dectobin.cpp

--- 3. Stack/Dec to Bin/dec2bin.cpp
+++ 3. Stack/Dec to Bin/dec2bin.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 using namespace std;
 
-
-
-
-
+void dectobin(int data)
+{
+    int rem,top=-1;
+    int stk[10];
+    while(data > 0)
+    {
+        rem = data%2;
+        data = data/2;
+        top++;
+        stk[top] = rem;
+    }
+    for(int i=top;i>=0;i--)
+        cout<<stk[i];
+}
 
 int main()
 {
+    cout<<"-------------# Decimal To Binary #------------"<<endl<<endl;
+    int data;
+    cout<<"Enter Decimal No.:"<<endl;
+    cin>>data;
+    cout<<endl<<"Binary Representation is: "<<endl;
+    dectobin(data);
     return 0;
 }
