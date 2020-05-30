53cdbf402c37f2d94254e0294eeed7b820bccb95
Gaurav <35771409+gauravgupta45@users.noreply.github.com>
2020-05-31 00:55:22 +0530

    Update graph_main.cpp

--- 8. Graph/graph_main.cpp
+++ 8. Graph/graph_main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <string>
+#include <list>
+#include <queue>
+#include <iterator>
+#include <cmath>
 using namespace std;
+
+// int c = 1;                     // This is for directed and undirected graph operation
+queue <char> f_q;
+// In C++ programming, this is a keyword that refers to the current instance/object of the class. There can be 3 main usage of this keyword in C++.
+
+// #1 It can be used to pass current object as a parameter to another method.
+// #2 It can be used to refer current class instance variable.
+// #3 It can be used to declare indexers.
+
+// Here we are using this pointer to refer to current class instance variable.
+
+// In BST, BFS is just a Level by Level Traversal, and DFS is just a Preorder Traversal
 
 class Node
 {
@@ -14,6 +31,10 @@ class Node
             this->next = NULL;
             this->adj = NULL;
         }
+        char getname(Node* t)
+        {
+            return t->vertex;
+        }
         friend class LinkedGraph;
 };
 
@@ -26,11 +47,12 @@ class LinkedGraph
             head = NULL;
         }
         void AddVertex(Node*);
-        int ShowVertex();
-        int AddAdj(char, char);
-        int ShowAdj(char);
+        void ShowVertex();
+        void AddAdj(char, char);
+        void ShowAdj(char);
+        void bfs();
+        void dfs();
 };
-
 
 void LinkedGraph :: AddVertex(Node* newnode)
 {
@@ -38,36 +60,37 @@ void LinkedGraph :: AddVertex(Node* newnode)
         head = newnode;
     else
     {
-        Node* temp = head;
+        Node* temp = new Node;
+        temp = head;
         while(temp->next!=NULL)
             temp=temp->next;
         temp->next = newnode;
     }
 }
 
-int LinkedGraph :: ShowVertex()
+void LinkedGraph :: ShowVertex()
 {
     if(head == NULL)
     {
-        cout<<endl<<"Graph is Empty! Add some vertices first."<<endl;
-        return 0;
+        cout << endl << "Graph is Empty! Add some vertices first." << endl;
+        return;
     }
+    cout << endl << "Nodes of Graph are: ";
     Node* temp = head;
-    cout<<endl<<"Nodes of Graph are: ";
     while(temp!=NULL)
     {
-        cout<<temp->vertex<<" ";
+        cout << temp->vertex << " ";
         temp = temp->next;
     }
-    cout<<endl;
+    cout << endl;
 }
 
-int LinkedGraph :: AddAdj(char Vertex, char Vertex_edge)
+void LinkedGraph :: AddAdj(char Vertex, char Vertex_edge)
 {
     if(head == NULL)
     {
-        cout<<endl<<"Graph is Empty! Add some vertices first."<<endl;
-        return 0;
+        cout <<endl <<"Graph is Empty! Add some vertices first." << endl;
+        return;
     }
     Node* temp1 = head;
     Node* temp2;
@@ -76,10 +99,11 @@ int LinkedGraph :: AddAdj(char Vertex, char Vertex_edge)
         temp1 = temp1->next;
         if(temp1 == NULL)
         {
-            cout <<endl << "No such Vertex " <<Vertex << " in the graph!" <<endl;
-            return 0;
+            cout << endl << "No such Vertex " << Vertex << " in the graph!" << endl;
+            return;
         }
     }
+
     if(temp1->adj == NULL)
     {
         Node* newnode = new Node(Vertex_edge);
@@ -93,96 +117,190 @@ int LinkedGraph :: AddAdj(char Vertex, char Vertex_edge)
         Node* newnode = new Node(Vertex_edge);
         temp2->next = newnode;
     }
+    // Comment the followed line if you want the graph to be undirected.
+
 }
-int LinkedGraph :: ShowAdj(char Vertex)
+
+void LinkedGraph :: ShowAdj(char Vertex)
 {
     if(head == NULL)
     {
-        cout<<endl<<"Graph is Empty! Add some vertices first."<<endl;
-        return 0;
+        cout << endl << "Graph is Empty! Add some vertices first."<< endl;
+        return;
     }
     Node* ptr = head;
-    while(ptr->vertex!=Vertex)
+    while(ptr->vertex != Vertex)
     {
         ptr = ptr->next;
         if(ptr == NULL)
         {
-            cout <<endl << "No such Vertex " <<Vertex << " in the graph!" <<endl;
-            return 0;
+            cout << endl << "No such Vertex " << Vertex << " in the graph!" << endl;
+            return;
         }
     }
     if(ptr->adj == NULL)
     {
-        cout <<endl << "No Adjecent of Vertex " <<Vertex << " is present in the graph!" <<endl;
-        return 0;
+        cout <<endl << "No Adjecent of Vertex " << Vertex << " is present in the graph!" << endl;
+        return;
     }
     ptr = ptr->adj;
-    cout<<endl<<Vertex<<"-> ";
+    cout << endl << "Adjecent of " << Vertex << " -> ";
     while(ptr!=NULL)
     {
-        cout<<ptr->vertex<<" ";
+        cout << ptr->vertex << " ";
         ptr = ptr->next;
     }
-    cout<<endl;
+    cout << endl;
 }
+
+void LinkedGraph :: bfs()
+{
+    // Get the number of vertices (n) in Graph, so that we can create a queue of that size.
+    int n = 0;
+    Node* ptrforsize = head;
+    while(ptrforsize != NULL)
+    {
+        ptrforsize = ptrforsize->next;
+        n++;
+    }
+    int visited[n] = {0};
+
+    // Getting the user defined start point.
+    char start_data;
+    cout << endl << "Enter Starting Vertex: ";
+    cin >> start_data;
+    // Inserting the start vertex in the queue.
+    f_q.push(start_data);
+
+    // Find the index of current vertex, so that we can mark it visited in visited array.
+    Node* ptrforindex = head;
+    int index = 0;
+    while(ptrforindex->vertex != start_data)
+    {
+        ptrforindex = ptrforindex->next;
+        index++;
+    }
+    visited[index] = 1;
+
+    // Traversing the whole graph from the start node, which user entered, untill the queue becomes empty.
+    cout << endl << "BFS traversal is: ";
+    while(!f_q.empty())
+    {
+        Node* ptrforvertex = head;
+        cout << f_q.front() << " ";
+
+        while(ptrforvertex->vertex != f_q.front())
+            ptrforvertex = ptrforvertex->next;
+
+        //Removing the front elem as we have now address of its adj elemnts.
+        f_q.pop();
+
+        // Traversing through the adjecent of front elem of queue considering it has atleast 1 adj. And put all its adj to the queue and there index too in visited array.
+        ptrforvertex = ptrforvertex->adj;
+        while(ptrforvertex != NULL)
+        {
+            // Finding the index of current vertex, so that we can put it into visited array. Basically we are providing each node an index, which can be used to diffrentiate them.
+            ptrforindex = head;
+            index = 0;
+            while(ptrforindex->vertex != ptrforvertex->vertex)
+            {
+                index++;
+                ptrforindex = ptrforindex->next;
+            }
+
+            // Checking if the index of current elem that we got from above, is in the visited arr or not.
+            if(visited[index] == 0)
+            {
+                visited[index] = 1;
+                f_q.push(ptrforvertex->vertex);
+            }
+            ptrforvertex = ptrforvertex -> next;
+        }
+    }
+    cout << endl;
+}
+
+
+void LinkedGraph :: dfs()
+{
+    cout << endl << "Wait for few days :)" << endl;
+}
+
 
 int main()
 {
     LinkedGraph obj1;
     Node* n;
-    int size1,size2,ch;
-    char val,origin,adj_elem,x;
 
     cout<<"------------------ #MENU# ---------------------"<<endl;
-
+    int ch;
     do
     {
-        cout<<endl<<"1. Add Vertex"<<endl<<"2. Add Adjecent"<<endl<<"3. Show Vertices"<<endl<<"4. Show Adjecents"<<endl<<"5. Delete"<<endl<<"6. Exit"<<endl<<endl;
-        cin>>ch;
+
+    char val,origin,adj_elem,x;
+        cout<<endl<<"1. Add Vertex"<<endl<<"2. Add Adjecent"<<endl<<"3. Show Vertices"<<endl<<"4. Show Adjecents"<<endl<<"5. BFS Traversal"<<endl<<"6. DFS Traversal"<<endl<<"7. Exit"<<endl<<endl;
+        cin >> ch;
         switch(ch)
         {
         case 1:
-            //cout<<endl<<"Enter Total No. Vertex to insert in Graph: ";
-            //cin>>size1;
             cout<<"Enter Vertex Data: ";
-            cin>>val;
+            cin >> val;
             n = new Node(val);
             obj1.AddVertex(n);
-            /*for(int i=0;i<size1;i++)
-            {
-                cin>>val;
-                n = new Node(val);
-                obj1.AddNode(n);
-            }
-            */
             break;
+
         case 2:
             cout<<endl<<"Insert Adjecent at: ";
-            cin>>origin;
-            //cout<<"Total Edge Elements to be inserted: ";
-            //cin>>size2;
-            cout<<"Enter Adjecent Data: ";
-            cin>>adj_elem;
+            cin >> origin;
+            cout << "Enter Adjecent Data: ";
+            cin >> adj_elem;
             obj1.AddAdj(origin,adj_elem);
-            /*for(int i=0;i<size2;i++)
-            {
-                cin>>edge_elem;
-                obj1.AddEdge(origin,edge_elem);
-            }
-            */
+            obj1.AddAdj(adj_elem,origin);       // This is done to make the graph directed.
             break;
+
         case 3:
             obj1.ShowVertex();
             break;
+
         case 4:
             cout<<endl<<"Show Adjecent of: ";
             cin>>x;
             obj1.ShowAdj(x);
             break;
+
         case 5:
-            cout<<endl<<"Under Construction!"<<endl;
+            cout << endl << "BFS Traversal is :"<< endl;
+            obj1.bfs();
+            break;
+
+        case 6:
+            cout << endl << "DFS Traversal is :"<< endl;
+            obj1.dfs();
             break;
         }
-    }while(ch!=6);
+    }while(ch!=7);
+
+    // n = new Node('H');
+    // obj1.AddVertex(n);
+    // n = new Node('B');
+    // obj1.AddVertex(n);
+    // n = new Node('D');
+    // obj1.AddVertex(n);
+    // n = new Node('M');
+    // obj1.AddVertex(n);
+    //
+    // obj1.AddAdj('H','B');
+    // obj1.AddAdj('H','M');
+    // obj1.AddAdj('H','D');
+    //
+    // obj1.ShowVertex();
+    //
+    // obj1.ShowAdj('H');
+    // obj1.ShowAdj('B');
+    // obj1.ShowAdj('D');
+    // obj1.ShowAdj('M');
+    //
+    // obj1.bfs();
+
     return 0;
 }
